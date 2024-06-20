103df8147f8209d31cd6259f8d89cb8577bd9da8
MrArcano <salvatore-dario@hotmail.it>
2024-06-20 16:39:13 +0200

    [add]: CORS_ALLOW_ALL_ORIGINS

--- server_slot_django/settings.py
+++ server_slot_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'slot_machine_api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'server_slot_django.urls'
@@ -126,3 +128,28 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Permetti tutte le origini
+CORS_ALLOW_ALL_ORIGINS = True
+
+# Oppure specifica le origini consentite
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     "http://example.com",
+# ]
+
+# Opzioni aggiuntive
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+]
+
